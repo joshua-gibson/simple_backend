3d71e6cef0a0a58d5dea03c53485fc666b3498c2
joshua-gibson <flash50k@gmail.com>
2019-10-15 10:49:02 +0100

    added products

---
+++ Domain/Models/Product.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace simple_backend.Domain.Models
+{
+    public class Product
+    {
+        public int Id {get; set;}
+        public string Name {get; set;}
+        public short QuantityInPackage {get; set;}
+        public EUnitOfMeasurement UnitOfMeasurement{get; set;}
+
+        public int CategoryId {get; set;}
+        public Category Category {get; set;}
+
+    }
+}
